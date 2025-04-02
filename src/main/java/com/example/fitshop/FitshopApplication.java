05514c222800d89bd4ea51d7fc1081d0385c72e0
Marek Koprek <245847@edu.p.lodz.pl>
2025-04-02 09:23:25 +0200

    spring boot init

---
+++ src/main/java/com/example/fitshop/FitshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitshopApplication.class, args);
+	}
+
+}
